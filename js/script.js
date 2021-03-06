99a5b910e2392436e3eeedf03156a40f017cc6e6
azatbais <azat.baisynov@gmail.com>
2021-03-06 13:57:18 +0600

    initial

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let input = document.querySelector('.slider')
+let btns = document.querySelectorAll('.btn')
+let img = document.querySelector('img')
+
+let change = 0;
+btns.forEach( el => {
+    el.addEventListener('click', () => {
+        switch (el.textContent) {
+            case 'Blur' :
+                change = 0
+                break
+            case 'Sepia' :
+                change = 1
+                break
+            case 'Invert' :
+                change = 2
+                break
+        }
+    })
+})
+
+input.addEventListener('input', () => {
+    switch (change) {
+        case 0 :
+            img.style = `filter: blur(${input.value}px);`
+            break
+        case 1 :
+            img.style = `filter: grayscale(${input.value}%);`
+            break
+        case 2 :
+            img.style = `filter: invert(${input.value}%);`
+            break
+    }
+})
+
+console.dir(btns)
